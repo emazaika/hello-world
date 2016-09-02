af2261ecfbf80f9af74a8b732bfe3ae4c6c14dff
Erica Mazaika <ericamazaika@Ericas-MacBook-Air.local>
2016-09-02 09:07:33 -0400

    remove alignIO format, print simple

--- 20160825_test9.pl
+++ 20160825_test9.pl
@@ -64,13 +64,13 @@ chomp($homology_input);
 my $selectedhomology = $homologies->[$homology_input];
 my $simplealign = $selectedhomology->get_SimpleAlign();
 
-my $alignIO = Bio::AlignIO->newFh(
-    -interleaved => 0,
-    -fh => \*STDOUT,
-    -format => "clustalw",
-    -idlength => 20);
+#my $alignIO = Bio::AlignIO->newFh(
+#    -interleaved => 0,
+#    -fh => \*STDOUT,
+#    -format => "clustalw",
+#    -idlength => 20);
 
-print $alignIO $simple_align;
+print $simple_align;
 
 #my $homology = $homologies->[0];
 #print ref($homologies);
